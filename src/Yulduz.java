d24c65fd125204a863a16ee9db108520a5629622
yulduzakabirova <akabirovayulduz@gmail.com>
2020-08-02 11:30:07 -0400

    print lastname

--- src/Yulduz.java
+++ src/Yulduz.java
@@ -1,6 +1,8 @@
 public class Yulduz{
     public static void main(String[] args) {
-        SYstem.out.println("Yulduz!");
+        System.out.println("Yulduz!");
+        System.out.println("Akabirova!");
+
 
     }
 }
